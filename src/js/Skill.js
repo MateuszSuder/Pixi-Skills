88b60accb04e4d811d71cdb7d8fadaaf55180ec1
Mateusz Suder <matisu999@gmail.com>
2020-09-09 16:12:46 +0200

    Deleted onclick, added methods to make resize easier

--- src/js/Skill.js
+++ src/js/Skill.js
@@ -1,62 +1,53 @@
 import { skillsContainer, size, list } from './index';
 import { randomInt } from './functions';
-import { Sprite, Ticker } from 'pixi.js-legacy'
-
-
-export class Skill {
-    sprite: Sprite;
-    startingPos: number[] = []; // x, y
-    multiSin: number = 0; // 15 - 50
-    multiIt: number = 0; // 0.025 - 0.05
-    it: number = 0;
-    floating: Ticker = new Ticker();
-    ratio: number = 1;
-
-    constructor(s: Sprite) {
+import { Ticker } from 'pixi.js-legacy';
+var Skill = /** @class */ (function () {
+    function Skill(s) {
+        var _this = this;
+        this.startingPos = []; // x, y
+        this.multiSin = 0; // 15 - 50
+        this.multiIt = 0; // 0.025 - 0.05
+        this.it = 0;
+        this.floating = new Ticker();
+        this.ratio = 1;
         this.sprite = s;
         skillsContainer.addChild(this.sprite);
-
         this.sprite.anchor.set(0.5, 0.5);
-
-        this.setSize()
-
-        this.setMultis()
-
+        this.setSize();
+        this.setMultis();
         this.setPosition();
-
-        this.floating.add((delta) => {
-            this.sprite.position.y = this.startingPos[1] + Math.sin(this.it) * this.multiSin;
-            this.it += this.multiIt;
+        this.floating.add(function (delta) {
+            _this.sprite.position.y = _this.startingPos[1] + Math.sin(_this.it) * _this.multiSin;
+            _this.it += _this.multiIt;
         });
-
         this.floating.start();
-
     }
-
-    setMultis(){
+    Skill.prototype.setMultis = function () {
         this.multiSin = randomInt(window.innerHeight / 3 - size / 2, window.innerHeight / 2 - size / 2);
         this.multiIt = randomInt(25, 50) / 1000;
-    }  
-
-    setSize(){
+    };
+    Skill.prototype.setSize = function () {
         if (this.sprite.width > this.sprite.height) {
             this.ratio = this.sprite.height / this.sprite.width;
             this.sprite.width = size;
             this.sprite.height = size * this.ratio;
-        } else {
+        }
+        else {
             this.ratio = this.sprite.width / this.sprite.height;
             this.sprite.height = size;
             this.sprite.width = size * this.ratio;
         }
-    }
-
-    setPosition(){
+    };
+    Skill.prototype.setPosition = function () {
         this.sprite.position.y = window.innerHeight / 2;
-        if(this.startingPos.length == 0){
+        if (this.startingPos.length == 0) {
             this.sprite.position.x = size * (skillsContainer.children.length - 1) + size / 2;
-        }else{
+        }
+        else {
             this.sprite.position.x = size * (list.indexOf(this)) + size / 2;
         }
         this.startingPos = [this.sprite.position.x, this.sprite.position.y];
-    }
-}
+    };
+    return Skill;
+}());
+export { Skill };
