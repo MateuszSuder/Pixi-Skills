cd6ccd9f26494c54008fa9283dbde149e61e7304
Mateusz Suder <matisu999@gmail.com>
2020-09-09 16:13:32 +0200

    Added resizing, deleted unwanted variables and functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,68 +1,51 @@
-import { Application, Sprite, Ticker, Container } from "pixi.js-legacy";
+import { Application, Sprite, Container } from "pixi.js-legacy";
+import { Skill } from "./Skill";
 import { randomInt } from "./functions";
-var app = new Application({ transparent: true });
+export var app = new Application({ transparent: true });
 document.body.appendChild(app.view);
 app.loader.baseUrl = "src/img/";
-app.loader.add("Angular", "Angular.png")
-    .add("C++", "cpp.png")
+app.loader
+    .add("Angular", "Angular.png")
     .add("CSS3", "CSS3.png")
+    .add("Git", "Git.png")
     .add("HTML5", "HTML5.png")
-    .add("Java", "Java.png")
     .add("jQuery", "jQuery.png")
     .add("JavaScript", "js.png")
-    .add("MySQL", "mySQL.png")
-    .add("Node.js", "node.png")
     .add("npm", "npm.png")
-    .add("PHP", "PHP.png")
     .add("PixiJS", "pixi.png")
     .add("Sass", "SASS.png")
     .add("TypeScript", "ts.png")
     .add("webpack", "webpack.png");
 app.loader.onComplete.add(doneLoading);
 app.loader.load();
-var Skill = /** @class */ (function () {
-    function Skill(s, h) {
-        var _this = this;
-        this.it = 0;
-        this.floating = new Ticker();
-        this.high = h;
-        this.sprite = s;
-        this.multiSin = randomInt(15, 50);
-        this.multiIt = randomInt(25, 50) / 1000;
-        this.sprite.anchor.set(0.5, 0.5);
-        this.sprite.position.x = randomInt(this.sprite.width, window.innerWidth - this.sprite.width);
-        this.sprite.position.y = randomInt(this.sprite.height + this.multiSin, window.innerHeight - this.sprite.height - this.multiSin);
-        this.startingPos = [this.sprite.position.x, this.sprite.position.y];
-        this.floating.add(function (delta) {
-            _this.sprite.position.y =
-                _this.startingPos[1] + Math.sin(_this.it) * _this.multiSin;
-            _this.it += _this.multiIt;
-        });
-        this.floating.start();
-    }
-    return Skill;
-}());
-var skillsL = [];
-var skillsH = [];
+export var size = 0;
+function setSize() {
+    size = (window.innerWidth / Object.keys(app.loader.resources).length);
+}
+export var skillsContainer = new Container();
+export var list = [];
 function doneLoading() {
     resize();
-    console.log(Object.keys(app.loader.resources).length);
-    var highContainer = new Container();
-    var lowContainer = new Container();
+    setSize();
+    var sprites = [];
     for (var i in app.loader.resources) {
         var sprite = Sprite.from(app.loader.resources[i].texture);
-        sprite.interactive = true;
-        sprite.cursor = 'pointer';
-        if (i != 'C++' && i != 'Java' && i != 'PHP' && i != 'MySQL' && i != 'Node.js') {
-            skillsH.push(new Skill(sprite, true));
-        }
-        else {
-            skillsL.push(new Skill(sprite, false));
-        }
-        app.stage.addChild(sprite);
+        sprites.push(sprite);
     }
-}
-window.addEventListener("resize", resize);
-function resize() {
-    app.renderer.resize(window.innerWidth, window.innerHeight);
+    for (var i = 0; i < Object.keys(app.loader.resources).length; i++) {
+        var temp = randomInt(0, sprites.length - 1);
+        list.push(new Skill(sprites[temp]));
+        sprites.splice(temp, 1);
+    }
+    app.stage.addChild(skillsContainer);
+    window.addEventListener("resize", resize);
+    function resize() {
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+        setSize();
+        list.forEach(function (el) {
+            el.setSize();
+            el.setMultis();
+            el.setPosition();
+        });
+    }
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import { Application, Sprite, Container, Loader, Ticker, filters } from "pixi.js-legacy";
 import { Skill } from "./Skill";
+import { randomInt } from "./functions";
 
 export const app = new Application({ transparent: true });
 
@@ -9,90 +10,60 @@ app.loader.baseUrl = "src/img/";
 
 app.loader
   .add("Angular", "Angular.png")
-  .add("C++", "cpp.png")
   .add("CSS3", "CSS3.png")
+  .add("Git", "Git.png")
   .add("HTML5", "HTML5.png")
-  .add("Java", "Java.png")
   .add("jQuery", "jQuery.png")
   .add("JavaScript", "js.png")
-  .add("MySQL", "mySQL.png")
-  .add("Node.js", "node.png")
   .add("npm", "npm.png")
-  .add("PHP", "PHP.png")
   .add("PixiJS", "pixi.png")
   .add("Sass", "SASS.png")
   .add("TypeScript", "ts.png")
   .add("webpack", "webpack.png");
 
-export let bigger = new Loader();
-bigger.baseUrl = "src/img/400px";
-bigger.add("Angular400px", "Angular.png");
-
-bigger.load();
-
 app.loader.onComplete.add(doneLoading);
 app.loader.load();
 
-let skillsL: Skill[] = [];
-let skillsH: Skill[] = [];
-
-export let highContainer: Container = new Container();
-export let lowContainer: Container = new Container();
-
 export let size: number = 0;
 
-if (window.innerWidth > window.innerHeight) {
-  size = window.innerWidth / 20;
-} else {
-  size = window.innerHeight / 20;
+function setSize(){
+  size = (window.innerWidth / Object.keys(app.loader.resources).length);
 }
 
-export function blurScene(){
-  const blur = new filters.BlurFilter();
-  blur.blur = 0;
-  highContainer.filters = [blur];
-  lowContainer.filters = [blur];
-  let it = 0;
-  const bluring = new Ticker();
-  bluring.add((delta) => {
-    blur.blur += 0.5;
-    if(blur.blur >= 15){
-      bluring.destroy();
-    }
-  })
-  bluring.start();
-}
+
+export let skillsContainer: Container = new Container();
+export let list: Skill[] = [];
 
 function doneLoading() {
-    resize();
+  resize();
 
-    if (window.innerWidth > window.innerHeight) {
-        highContainer.position.x = highContainer.position.y = 0;
-        lowContainer.position.x = window.innerWidth / 2;
-        lowContainer.position.y = 0;
-    } else {
-        highContainer.position.x = highContainer.position.y = 0;
-        lowContainer.position.x = 0;
-        lowContainer.position.y = window.innerHeight / 2;
-    }
+  setSize();
 
-    for (let i in app.loader.resources) {
-        let sprite: Sprite = Sprite.from(app.loader.resources[i].texture);
-        sprite.interactive = true;
-        sprite.cursor = "pointer";
+  let sprites: Sprite[] = []
+  for (let i in app.loader.resources) {
+    let sprite: Sprite = Sprite.from(app.loader.resources[i].texture);
+    sprites.push(sprite)
+  }
+  for(let i = 0; i < Object.keys(app.loader.resources).length; i++){
+    let temp = randomInt(0, sprites.length - 1);
+    list.push(new Skill(sprites[temp]));
+    sprites.splice(temp, 1)
+  }
 
-        if (i != "C++" && i != "Java" && i != "PHP" && i != "MySQL" && i != "Node.js") {
-            skillsH.push(new Skill(sprite, true, i));
-        } else {
-            skillsL.push(new Skill(sprite, false, i));
-        }
+  app.stage.addChild(skillsContainer);
 
-        app.stage.addChild(highContainer, lowContainer);
-    }
+  window.addEventListener("resize", resize);
+
+  function resize() {
+    app.renderer.resize(window.innerWidth, window.innerHeight);
+    setSize();
+    list.forEach(el => {
+      el.setSize();
+      el.setMultis();
+      el.setPosition();
+    }) 
+  }
+
 }
 
-window.addEventListener("resize", resize);
 
-function resize() {
-  app.renderer.resize(window.innerWidth, window.innerHeight);
-}
